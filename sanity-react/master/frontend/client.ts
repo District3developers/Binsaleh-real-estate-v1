a9db41a0f188a59c018356f21a63d95092075c4c
District3developers <district3dev@gmail.com>
2023-07-20 15:41:31 +0500

    initial commit

--- sanity-react/master/frontend/client.ts
+++
@@ -1,7 +0,0 @@
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-  projectId: 'fnxwo3g6', // you can find this in sanity.json
-  dataset: 'production', // or the name you chose in step 1
-  useCdn: false // `false` if you want to ensure fresh data
-})
--- sanity-react/master/studio/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'fnxwo3g6',
-    dataset: 'production'
-  }
-})
